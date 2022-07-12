18d37b5255428d6bd2a4cdcb7101768137fa460a
Mali Matiso <s220651337@mandela.ac.za>
2022-07-12 23:17:41 +0200

    Formatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //importing required libraries
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
 import twitterLogo from "./assets/twitter-logo.svg";
 import * as fcl from "@onflow/fcl";
@@ -18,25 +18,26 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 function App() {
 
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <div className="logo-container">
-            <img src="./logo.png" className="flow-logo" alt="flow logo"/>
-            <p className="header">✨Awesome NFTs on Flow ✨</p>
-          </div>
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="header-container">
+                    <div className="logo-container">
+                        <img src="./logo.png" className="flow-logo" alt="flow logo"/>
+                        <p className="header">✨Awesome NFTs on Flow ✨</p>
+                    </div>
 
-          <p className="sub-text">The easiest NFT mint experience ever!</p>
-        </div>
+                    <p className="sub-text">The easiest NFT mint experience ever!</p>
+                </div>
 
-        <div className="footer-container">
-            <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-            <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
+                <div className="footer-container">
+                    <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo}/>
+                    <a className="footer-text" href={TWITTER_LINK} target="_blank"
+                       rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
