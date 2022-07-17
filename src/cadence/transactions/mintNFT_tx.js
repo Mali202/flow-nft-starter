ae32e85754c22b218c1fc47d522f918ff86c8f8b
Mali Matiso <s220651337@mandela.ac.za>
2022-07-18 01:00:17 +0200

    Update mint transaction to incorporate MetaDataViews

--- src/cadence/transactions/mintNFT_tx.js
+++ src/cadence/transactions/mintNFT_tx.js
@@ -1,7 +1,8 @@
 export const mintNFT = 
 `
-import BottomShot from 0x6d298508daa1ea0a 
+import Creed from 0x6d298508daa1ea0a 
 import NonFungibleToken from 0x631e88ae7f1d7c20
+import MetadataViews from 0x631e88ae7f1d7c20
 
 transaction(
   recipient: Address,
@@ -10,20 +11,20 @@ transaction(
   thumbnail: String,
 ) {
   prepare(signer: AuthAccount) {
-    if signer.borrow<&BottomShot.Collection>(from: BottomShot.CollectionStoragePath) != nil {
+    if signer.borrow<&Creed.Collection>(from: Creed.CollectionStoragePath) != nil {
       return
     }
 
     // Create a new empty collection
-    let collection <- BottomShot.createEmptyCollection()
+    let collection <- Creed.createEmptyCollection()
 
     // save it to the account
-    signer.save(<-collection, to: BottomShot.CollectionStoragePath)
+    signer.save(<-collection, to: Creed.CollectionStoragePath)
 
     // create a public capability for the collection
-    signer.link<&{NonFungibleToken.CollectionPublic}>(
-      BottomShot.CollectionPublicPath,
-      target: BottomShot.CollectionStoragePath
+    signer.link<&{NonFungibleToken.CollectionPublic, MetadataViews.ResolverCollection}>(
+      Creed.CollectionPublicPath,
+      target: Creed.CollectionStoragePath
     )
   }
 
@@ -31,12 +32,12 @@ transaction(
   execute {
     // Borrow the recipient's public NFT collection reference
     let receiver = getAccount(recipient)
-      .getCapability(BottomShot.CollectionPublicPath)
+      .getCapability(Creed.CollectionPublicPath)
       .borrow<&{NonFungibleToken.CollectionPublic}>()
       ?? panic("Could not get receiver reference to the NFT Collection")
 
     // Mint the NFT and deposit it to the recipient's collection
-    BottomShot.mintNFT(
+    Creed.mintNFT(
       recipient: receiver,
       name: name,
       description: description,
